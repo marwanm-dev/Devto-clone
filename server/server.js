63853881122d0b07639675cddcd7fcf921d1e389
marodevv <marodevv@gmail.com>
2022-09-14 14:20:49 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,6 +47,8 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use(cookieParser());
 
+app.use('/', express.static('build'));
+
 // Routes
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
