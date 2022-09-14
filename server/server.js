412d41da1973b45a7cafebdb77fadddfa573ee60
marodevv <marodevv@gmail.com>
2022-09-15 00:12:07 +0200

    ok

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.use('/users', require('./routes/users'));
 app.use('/posts', require('./routes/posts'));
 app.use('/comments', require('./routes/comments'));
 app.use('/tags', require('./routes/tags'));
-ost;
+
 mongoose.connection.once('open', () => {
   const io = new Server(httpServer, { cors: corsOptions });
 
