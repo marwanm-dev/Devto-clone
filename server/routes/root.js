2f9782968c6aa74423012da95d2df16fd2e44d36
marodevv <marodevv@gmail.com>
2022-09-15 19:44:45 +0200

    beautified server root

--- server/routes/root.js
+++ server/routes/root.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
-router.get('/', (req, res) => res.send('Dev.to clone server is running by marodevv'));
+router.get('^/$|/index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
 
 module.exports = router;
